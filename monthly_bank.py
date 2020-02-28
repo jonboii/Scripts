6ad7127beb2d3f61006d3f2eaab89acefcf218a3
Jon <jonathan.p.alvin93@gmail.com>
2020-02-28 15:01:10 -0500

    displayed csv w/o ck number and confirmation number

--- monthly_bank.py
+++ monthly_bank.py
@@ -1,5 +1,7 @@
-import pandas
+import pandas as pd
 
-pandas.read_csv('history.csv')
+statement = pd.read_csv('/home/prism/Desktop/history.csv', engine='python')
 
+statement.drop(['Confirmation', 'Check Number'], axis=1, inplace=True)
 
+print (statement.head())
