536500db627b9735ba72e20377bf5b0f90b9868b
jonboii <51348628+jonboii@users.noreply.github.com>
2020-03-11 15:17:17 -0400

    Update monthly_bank.py
    i don't think there are any changes to this.

--- monthly_bank.py
+++ monthly_bank.py
@@ -7,6 +7,8 @@ import shutil
 
 statement = pd.read_csv('A:\Jonathan\Downloads\history.csv', engine='python')
 statement.drop(['Confirmation', 'Check Number'], axis=1, inplace=True,)
+
+#Trying to remove transsfer deposit and cash withdrawal
 statement.drop(['Transfer Deposit','Cash Withdrawal'], ,inplace=True])
 
 
