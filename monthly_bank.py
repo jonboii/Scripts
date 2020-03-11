0d7cbba5d0d3c75d5a590a475b8229c2144f3ba6
jonboii <51348628+jonboii@users.noreply.github.com>
2020-03-11 16:17:16 -0400

    still trying to delete
    Still trying to delete transfer deposite, etc

--- monthly_bank.py
+++ monthly_bank.py
@@ -6,10 +6,10 @@ import shutil
 
 
 statement = pd.read_csv('A:\Jonathan\Downloads\history.csv', engine='python')
-statement.drop(['Confirmation', 'Check Number'], axis=1, inplace=True,)
+statement.drop(['Confirmation', 'Check Number', 'Unnamed: 5'], axis=1, inplace=True)
 
 #Trying to remove transsfer deposit and cash withdrawal
-statement.drop(['Transfer Deposit','Cash Withdrawal'], ,inplace=True])
-
+s = pd.Series()
+s.str.lstrip([Transfer])
 
 print (statement.head())
