750d329c4cd90823127bd26c0b11d570644c8df0
jonboii <51348628+jonboii@users.noreply.github.com>
2022-07-01 11:48:38 -0400

    Update book_webscrape.py

--- book_webscrape.py
+++ book_webscrape.py
@@ -3,27 +3,32 @@ import urllib.request
 import time
 import os
 from bs4 import BeautifulSoup
+#TODO: pull from csv/word/make list to integrate for search terms
+#TODO: 
 
 
-searchterm = testing
-
-#url = 'https://polaris.hclibrary.org/polaris/search/'
-url = 'https://polaris.hclibrary.org/polaris/search/searchresults.aspx?ctx=1.1033.0.0.3&type=Default&term=testing&by=KW&sort=MP&limit=TOM=*&query=&page=0&searchid=1'
-
-response = requests.get(url)
-html = response.text
-print(response)
-
-soup = BeautifulSoup(html, "html.parser")
-soup.a
 
 
-def getbooks():
-    
+url = 'https://polaris.hclibrary.org/polaris/search/searchresults.aspx?ctx=1.1033.0.0.3&type=Default&term=hello&by=KW&sort=MP&limit=TOM=*&query=&page=0&searchid=1'
+
+
+response = str(requests.get(url))
+
+#This will check to see if the url is available by getting the correct 200 response from the web server
+#if response[11:14].string == '200':
+#    result = requests.get(url)
+#    print(result.text)
+
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+title = doc.find_all('testing')
+print(title)
+
 
 
 
 #if onshelf == True:
 #    print('Available -> ' + booktitle)
 
-xpathonshelf = /html/body/div[2]/div/table/tbody/tr[11]/td[3]
+###xpathonshelf = /html/body/div[2]/div/table/tbody/tr[11]/td[3]###
